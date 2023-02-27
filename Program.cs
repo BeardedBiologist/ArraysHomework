69c00f9c4d903315f6deb6c1136c9f2eb63c8f07
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-02-27 15:03:29 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿/* BREIF
+ * 
+ * Create an array of 3 names. Ask the user which number to select. When the 
+ * user gives you a number, return that name. Make sure to check for invalid 
+ * numbers.
+ *
+ */
