9484d194d5f3091b6801089486022283550f9a85
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-02-28 10:05:30 +0100

    Setup the console app. Bugs still on index.

--- Program.cs
+++ Program.cs
@@ -5,3 +5,34 @@
  * numbers.
  *
  */
+string[] firstNames = new string[] { "Corey", "Smith", "Jones" };
+string inputStatus;
+bool exitStatus = false;
+
+do
+{
+    Console.Write("The array holds 3 names. Type 1, 2 or 3 and press enter: ");
+    string indexStr = Console.ReadLine();
+    int.TryParse(indexStr, out int index);
+    int newIndex = index - 1;
+
+    if (newIndex >= 0 && newIndex <= 3)
+    {
+        Console.WriteLine($"The name you selected was: {firstNames[newIndex]}");
+    }
+    else
+    {
+        Console.WriteLine("Index was out of range, or doesn't exist");
+    }
+
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.Write("Type Exit to close, or press ENTER to continue");
+    inputStatus = Console.ReadLine();
+    if (inputStatus.ToLower() == "exit")
+    {
+        exitStatus = true;
+    }
+
+} while (exitStatus == false);
